10a49033622c5dedcf0fd6280bc4bc7d06501a82
Knirb <viktorasbrink@gmail.com>
2019-05-07 11:56:17 +0200

    literally dont remember, best commit E

--- gui.py
+++ gui.py
@@ -1,8 +1,14 @@
 from tkinter import *
+
 from tkinter import scrolledtext
+import tkinter as tk
 root = Tk()
 root.title('Word predictor')
 root.geometry('400x300')
-textBox = scrolledtext.ScrolledText(root, width=49, height=5)
+textBox = scrolledtext.ScrolledText(root, width=49, height=5, wrap   = 'word')
+listBox = Listbox(root,width=49, height=5)
+textBox.insert(tk.INSERT, "testing")
+print(textBox.get("wordstart", tk.END))
 textBox.grid(column = 1, row = 0)
+listBox.grid(column = 1, row = 2)
 root.mainloop()
