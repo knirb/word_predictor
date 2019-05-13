641e6ac29208ca2bd2d1fe818696e4abe482e6b2
Knirb <viktorasbrink@gmail.com>
2019-05-13 14:29:19 +0200

    add threading and connect main +gui

--- Main.py
+++ Main.py
@@ -1,27 +1,39 @@
 from WordPredictor import WordPredictor
-
-def main():
+from gui import GUI
+import threading
+gui = GUI()
+def second_main():
     word_predictor = WordPredictor()
     word_predictor.process_file("corpus.txt")
-
-    user_input = "welcom"
+    user_input = gui.getText()
+    user_input = user_input[:-1]
+    print('user input: ' + user_input)
+    print('user input[-1]:' + user_input[-1] + 'end')
     user_word_list = user_input.split()
 
     # guess next full word using previous word
     if user_input[-1] == " " and len(user_word_list) == 1:
+        print('here1')
         word_predictor.create_bigram()
         guess_1, guess_2, guess_3 = word_predictor.guess_next_word_bigram(user_word_list[0])
 
     # guess next full word using past 2 words
     elif user_input[-1] == " " and len(user_word_list) >= 2:
         word_predictor.create_trigram()
+        print(user_word_list[-2], user_word_list[-1])
         guess_1, guess_2, guess_3 = word_predictor.guess_next_word_trigram(user_word_list[-2], user_word_list[-1])
 
     # guess ending of current word
     else:
         guess_1, guess_2, guess_3 = word_predictor.finish_word(user_word_list[-1].lower())
-
+    gui.setList([guess_1, guess_2, guess_3])
     print(guess_1, guess_2, guess_3)
+
+def main():
+    second_main_thread = threading.Thread(target = second_main)
+    second_main_thread.start()
+    gui.root.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- gui.py
+++ gui.py
@@ -25,20 +25,18 @@ class GUI:
         self.root = Tk()
         self.root.title('Word Predictor')
         self.root.geometry('400x300')
-        self.textBox = scrolledtext.ScrolledText(root, width=49, height=5, wrap   = 'word')
-        self.listBox = Listbox(root,width=49, height=5)
-        self.textBox.insert(tk.INSERT, "testing if this is \n working ")
+        self.textBox = scrolledtext.ScrolledText(self.root, width=49, height=5, wrap   = 'word')
+        self.listBox = Listbox(self.root,width=49, height=5)
+        self.textBox.insert(tk.INSERT, "This is our example text this is ")
         self.textBox.grid(column = 1, row = 0)
         self.listBox.grid(column = 1, row = 2)
-    def getText():
-        self.curText = textBox.get('1.0',tk.END)
-        tokens = tokenize(self.curText)
-        return tokens[len(tokens)-3:len(tokens)-1]
-    def setList(guesses): #TODO figure out listbox
-        pass
-
-
-    def tokenize(text_in): #TODO: write this function
+    def getText(self):
+        self.curText = self.textBox.get('1.0',tk.END)
+        return self.curText
+    def setList(self, guesses): #TODO figure out listbox
+        for guess in guesses:
+            self.listBox.insert(END, guess)
+    def tokenize(self, text_in): #TODO: write this function
         try :
             tokens = nltk.word_tokenize(text_in)
             return tokens
@@ -46,8 +44,8 @@ class GUI:
             nltk.download('punkt')
             tokens = nltk.word_tokenize(text_in)
             return tokens
-    def run_loop():
-        root.mainloop()        
+    def run_loop(self):
+        self.root.mainloop()
 
     # def keyCheck():
     #     while True:
@@ -68,20 +66,23 @@ class GUI:
     #         curText = textBox.get('1.0',tk.END)
     #     except:
     #         print('something went wrong')
+def run_process():
+    pass
 
-# def main():
-#     wp = WordPredictor()
-#     wp.process_file('corpus.txt')
-#     keyboardThread = threading.Thread(target = keyCheck)
-#     keyboardThread.start()
-#     root.mainloop()
-#     keyboardThread.join()
-# if __name__ == '__main__':
-#     try:
-#         main()
-#     except KeyboardInterrupt:
-#         print('Interrupted')
-#         try:
-#             sys.exit(0)
-#         except SystemExit:
-#             os._exit(0)
+def main():
+    wp = WordPredictor()
+    wp.process_file('corpus.txt')
+    gui = GUI()
+    operating_thread = threading.Thread(target = run_process)
+    keyboardThread.start()
+    gui.root.mainloop()
+    # keyboardThread.join()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
